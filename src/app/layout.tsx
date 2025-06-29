098d3251130736126a06919ca390563001c5f6d0
Soham Haldar <sohamhaldar25@gmail.com>
2025-06-29 20:04:50 +0530

    add Google Tag Manager integration to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -176,10 +176,29 @@ export default function RootLayout({
             }])
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NSTDJ77Q');
+            `,
+          }}
+        />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${rubik.variable} ${jost.variable} min-h-screen bg-background font-sans antialiased scrollbar-thin scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-thumb-gray-800/60`}
+        className={`${geistSans.variable} ${geistMono.variable} ${rubik.variable} ${jost.variable} min-h-screen bg-background font-sans antialiased scrollbar-thin scrollbar-track-transparent scrollbar-thumb-rounded-full scrollbar-thumb-gray-800/60`} 
       >
+        
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NSTDJ77Q"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}>
+            </iframe>
+          </noscript>
           <LoaderProvider>
           <AuthProvider>
             <PersonaProvider>
